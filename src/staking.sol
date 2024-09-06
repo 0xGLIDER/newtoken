01281190690c85f1ee41312132dd76ae87dcc1c9
0xglider <0xglider@proton.me>
2024-09-06 14:16:35 -0600

    update

--- src/staking.sol
+++ src/staking.sol
@@ -13,6 +13,15 @@ interface nftIface {
 
 interface IMintableToken is IERC20 {
     function mintTo(address recipient, uint256 amount) external;
+    function permit(
+        address owner, 
+        address spender, 
+        uint256 value, 
+        uint256 deadline, 
+        uint8 v, 
+        bytes32 r, 
+        bytes32 s
+    ) external;
 }
 
 /**
